bdcab2ce164f4ad5b2bfa65937155c376ff50b2a
Pranav Veldurthi <veldurthipranav@gmail.com>
2023-07-17 12:54:25 -0500

    Initial Commit

---
+++ Wlog/WlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WlogApp.swift
+//  Wlog
+//
+//  Created by Pranav on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
